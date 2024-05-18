0dfbcda9b9d4f49e0fd57d5e7582c0dc80a1d5c7
Joel Guillen <noctis2001@gmail.com>
2024-05-18 16:07:36 -0400

    Se agregaron clases de taildwind para hacer un poco más reponsive el login.

--- apps/client/src/pages/login-page/login.tsx
+++ apps/client/src/pages/login-page/login.tsx
@@ -79,12 +79,12 @@ function login (){
 
     return(
         <div className="container-login flex justify-center items-center w-screen h-screen">
-            <div className="card lg:card-side bg-base-100 shadow-xl h-4/6 w-1/2">
-                <div className="bg-gray-800 w-1/2 h-full image-login blur-sm">
-                
+            <div className="card xl:card-side bg-base-100 shadow-xl min-[320px]:h-3/4 min-[320px]:w-4/5 sm:h-3/4 lg:h-5/6 lg:w-1/2 xl:h-5/6 w-1/2">
+                <div className="bg-gray-800 w-1/2  image-login blur-sm">
+    
                 </div>
-                <div className="card-body w-1/2">
-                    <h1 className="text-center text-3xl text-white pb-3">INICIO DE SESIÓN </h1>
+                <div className="card-body xl:w-1/2">
+                    <h1 className="text-center sm:text-xl xl:text-3xl text-white pb-3">INICIO DE SESIÓN </h1>
                         <div className="card-actions flex flex-col items-center">
                             <form className="flex flex-col gap-2 w-full items-center" onSubmit={handleSubmit}>
                                 <label className="w-full text-white relative">
@@ -92,18 +92,20 @@ function login (){
                                     <div>
                                         <FontAwesomeIcon icon={faEnvelope} style={{color: colorIcons.usser,}} 
                                             className="absolute bottom-2 left-2 size-7" />
-                                        <input type="email" className="block input input-bordered w-full mt-1 pl-10" 
+                                        <input type="email" className="block input input-bordered w-full mt-1 pl-10 min-[320px]:text-xs lg:text-base text-base" 
                                             name="login-usser" autoComplete="off" onBlur={handleBlur}
                                         />
                                     </div>
                                    {iconValidation.usser === 'validated' ? (
                                         <FontAwesomeIcon icon={faCircleCheck} style={{color: "#1a890b"}} 
-                                        className="absolute bottom-4 left-80" 
+                                        className="absolute min-[320px]:bottom-4 min-[320px]:left-[174px] xl:bottom-4 xl:left-56 
+                                            lg:bottom-4 lg:left-96 2xl:bottom-4 2xl:left-80" 
                                         />
                                     ) : (
                                         iconValidation.usser === 'error' && (
                                             <FontAwesomeIcon icon={faCircleXmark} style={{color: "#000000"}} 
-                                            className="absolute bottom-4 left-80"
+                                            className="absolute min-[320px]:bottom-4 min-[320px]:left-[175px] xl:bottom-4 xl:left-56 
+                                                lg:bottom-4 lg:left-96 2xl:bottom-4 2xl:left-80"
                                             />
                                         )
                                     )}
@@ -118,19 +120,21 @@ function login (){
                                         <FontAwesomeIcon icon={faLock} style={{color: colorIcons.password,}} 
                                             className="absolute bottom-2 left-2 size-7"
                                         />   
-                                        <input type="password" className="block input input-bordered w-full mt-1 pl-10" 
+                                        <input type="password" className="block input input-bordered w-full mt-1 pl-10 min-[320px]:text-xs" 
                                             name="login-password" onBlur={handleBlur}/>  
                                     </div>
                                       {iconValidation.password === 'validated' ? (
                                         <FontAwesomeIcon icon={faCircleCheck} style={{color: "#1a890b"}} 
-                                            className="absolute bottom-4 left-80"
+                                            className="absolute min-[320px]:bottom-4 min-[320px]:left-[174px] xl:bottom-4 xl:left-56 
+                                                lg:bottom-4 lg:left-96 2xl:bottom-4 2xl:left-80"
                                         />
 
                                     ) : (
                                         iconValidation.password === 'error' && (
                                         <FontAwesomeIcon icon={faCircleXmark} style={{color: "#000000"}} 
-                                            className="absolute bottom-4 left-80"
-                                        />
+                                            className="absolute min-[320px]:bottom-4 min-[320px]:left-[175px] xl:bottom-4 xl:left-56 
+                                                lg:bottom-4 lg:left-96 2xl:bottom-4 2xl:left-80"
+                                        />  
 
                                         )
                                     )}
