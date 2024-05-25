17901316bba3260bdc9e801e5599bd5dce65fdd4
Joel Guillen <noctis2001@gmail.com>
2024-05-24 21:14:39 -0400

    Instalación de DaisyUi

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,9 @@
 import Index from "./pages/landing";
-
+import SignUp from "./pages/landing/sign-up/SignUpForm";
 export default function App() {
+  return(
+    <SignUp/>
+  )
 
-  return <Index/>
+
 }
