ce379b5d05f78cba6fcee65e166229b583c68dc5
Joel Guillen <noctis2001@gmail.com>
2024-06-10 19:56:01 -0400

    Creación del carrusel y el custom hook de citas "useQuote"

--- apps/client/src/components/Navbar.tsx
+++ apps/client/src/components/Navbar.tsx
@@ -1,31 +1,37 @@
+import IconAccount from "../pages/landing/account-svgrepo-com.svg"
+
 export function NavBar () {
   return(
-    <nav className="flex w-screen bg-[#00000033] h-20 items-center">
-      <div className="flex w-1/2 justify-start text-4xl text-white pl-12">
-        <span className="pl-2 font-bold">
+    <nav className="flex bg-[#1C232B] h-20 items-center">
+      <div className="flex w-1/2 justify-start text-4xl text-white ">
+        <span className="relative left-4 font-bold">
           <h2>
             Legendarium
           </h2>
         </span>
       </div>
 
-      <div className="flex w-1/2 justify-end text-xl gap-12 text-white ">
-        <a href="">
+      <div className="flex justify-end items-center text-xl text-white relative w-1/2 ">
+        <a href="" className="absolute right-96">
           <span>
               Inicio
           </span>
         </a>
-        <button>
+        <button className="absolute right-72">
           <span>
               Buscar
           </span>
         </button>
           
-        <a href="">
-          <span className="pr-2">
+        <a href="" className="absolute right-40">
+          <span className="">
               Contactos
           </span>
         </a>
+
+        <div>
+          <img src={IconAccount} alt="Profile" className="h-14 relative right-10 cursor-pointer" />
+        </div>
       </div>
   </nav>
   )
