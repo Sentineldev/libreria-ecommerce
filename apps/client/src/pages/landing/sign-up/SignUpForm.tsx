e50b0c65a25e71d8872067c7e1879721fbf80a6b
Joel Guillen <noctis2001@gmail.com>
2024-06-25 14:51:40 -0400

    Creación del componente LocationSelector, para elegir el pais, estado y ciudad.

--- apps/client/src/pages/landing/sign-up/SignUpForm.tsx
+++ apps/client/src/pages/landing/sign-up/SignUpForm.tsx
@@ -1,7 +1,15 @@
 import { Input } from "./components/Inputs"
 import BackGroundSignUp from "./pictures/young-woman-reading.jpg"
+import { LocationSelector } from "./components/LocationSelectors"
 
 function SignUp(){
+
+	function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+		e.preventDefault()
+		const fields = Object.fromEntries(new window.FormData(e.currentTarget))
+		console.log(fields)
+	}
+
     return(
 			<main className="w-screen h-screen"  style={{
 				backgroundImage: `url(${BackGroundSignUp})`,
@@ -11,45 +19,44 @@ function SignUp(){
 				<div className="w-full h-full  flex justify-center items-center">
 					<div className="bg-cover w-1/2 h-1/2" >
 						<h2 className="text-2xl text-center text-white">Registro de Usuarios</h2>
-						<form className="">
+						<form className="" onSubmit={handleSubmit}>
 							<div className="grid grid-cols-[1fr_1fr_1fr_1fr] grid-rows-[1fr_1fr_1fr_1fr_1fr_1fr] gap-2 text-white font-semibold">
-								<Input classNameLabel="flex flex-col text-center items-center w-full h-full" classNameContainer="pl-2 col-span-2" 
-									valueLabel="Nombres" inputType="text"
+								<Input classNameLabel="flex flex-col text-center items-center w-full" classNameContainer="pl-2 col-span-2" 
+									valueLabel="Nombres" inputType="text" nameInput="name"
 								/>
-								<Input classNameLabel="flex flex-col text-center items-center w-full h-full" classNameContainer="pr-2 col-span-2" 
-									valueLabel="Apellidos" inputType="text"
+								<Input classNameLabel="flex flex-col text-center items-center w-" classNameContainer="pr-2 col-span-2" 
+									valueLabel="Apellidos" inputType="text" nameInput="lastname"
 								/>
-								<Input classNameLabel="flex flex-col text-center items-center w-full h-full" classNameContainer="pl-2 col-span-full pr-2"	
-									valueLabel="Correo Electrónico" inputType="email"
+								<Input classNameLabel="flex flex-col text-center items-center w-full" classNameContainer="pl-2 col-span-full pr-2"	
+									valueLabel="Correo Electrónico" inputType="email" nameInput="email"
 								/>	
-								<Input classNameLabel="flex flex-col text-center items-center w-full h-full" classNameContainer="pl-2 col-span-2" 
-									valueLabel="Fecha de Nacimiento" inputType="date"
+								<Input classNameLabel="flex flex-col text-center items-center w-full" classNameContainer="pl-2 col-span-2" 
+									valueLabel="Fecha de Nacimiento" inputType="date" nameInput="birthday"
 								/>
-								<Input classNameLabel="flex flex-col text-center items-center w-full h-full" classNameContainer="pr-2 col-span-2" 
-									valueLabel="Género" inputType="text"
-								/>
-								<div className="col-span-full grid grid-cols-[1fr_1fr_1fr] gap-2">
-										<Input classNameLabel="flex flex-col text-center items-center w-full h-full" classNameContainer="pl-2 col-span-1" 
-										valueLabel="País" inputType="text"
-										/>
-										<Input classNameLabel="flex flex-col text-center items-center w-full h-full" classNameContainer="pl-2 col-span-1" 
-										valueLabel="Estado" inputType="text"
-										/>
-										<Input classNameLabel="flex flex-col text-center items-center w-full h-full" classNameContainer="pr-2 col-span-1" 
-										valueLabel="Ciudad" inputType="text"
-										/>
+							
+								<div className="pl-2 pr-2 col-span-2 flex items-end w-full">
+									<label className="w-full"> Género
+										<select className="block select select-primary w-full  h-14"  name="gender">
+											<option value="Masculino">Masculino</option>
+											<option value="Femenino">Femenino</option>
+											<option value="Otro">Otro</option>
+										</select>
+									</label>
 								</div>
-								<Input classNameLabel="flex flex-col text-center items-center w-full h-full" classNameContainer="pl-2 col-span-2" 
-									valueLabel="Dirección" inputType="text"
+								
+								<LocationSelector/>
+								
+								<Input classNameLabel="flex flex-col text-center items-center w-full" classNameContainer="pl-2 col-span-2" 
+									valueLabel="Dirección" inputType="text" nameInput="direction"
 								/>
-								<Input classNameLabel="flex flex-col text-center items-center w-full h-full" classNameContainer="pr-2 col-span-2"
-								 valueLabel="Código Postal" inputType="text"
+								<Input classNameLabel="flex flex-col text-center items-center w-full" classNameContainer="pr-2 col-span-2"
+								 valueLabel="Código Postal" inputType="text" nameInput="postal"
 								 />
-								<Input classNameLabel="flex flex-col text-center items-center w-full h-full" classNameContainer="pl-2 col-span-2" 
-									valueLabel="Contraseña" inputType="password"
+								<Input classNameLabel="flex flex-col text-center items-center w-full" classNameContainer="pl-2 col-span-2" 
+									valueLabel="Contraseña" inputType="password" nameInput="password"
 								/>
-								<Input classNameLabel="flex flex-col text-center items-center w-full h-full" classNameContainer="pr-2 col-span-2" 
-									valueLabel="Confirmar Contraseña" inputType="password"
+								<Input classNameLabel="flex flex-col text-center items-center w-full" classNameContainer="pr-2 col-span-2" 
+									valueLabel="Confirmar Contraseña" inputType="password" nameInput="confirm-password"
 								/>
 							</div>
 							<div className="h-24 w-full mt-1 gap-1 flex flex-col justify-center items-center">
--- apps/client/src/pages/landing/sign-up/components/Inputs.tsx
+++ apps/client/src/pages/landing/sign-up/components/Inputs.tsx
@@ -3,16 +3,17 @@ type PropsInputs = {
     classNameLabel: string
     valueLabel: string,
     inputType: string,
+    nameInput: string
     
 }
 
-export const Input = ({classNameContainer,classNameLabel, valueLabel, inputType}:PropsInputs) =>{
+export const Input = ({classNameContainer,classNameLabel, valueLabel, inputType, nameInput}:PropsInputs) =>{
     return(
         <div className={classNameContainer}>
             <div className="w-full h-full">
                 <label className={classNameLabel}>
                     {valueLabel}
-                    <input type={inputType} className="w-full input input-bordered input-primary h-full" />
+                    <input type={inputType} className="w-full input input-bordered input-primary  h-14" name={nameInput} />
                 </label>
             </div>
         </div>
