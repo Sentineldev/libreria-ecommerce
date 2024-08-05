41d2c023627c0c11c9a3297c0c239963e5b900ab
Joel Guillen <noctis2001@gmail.com>
2024-08-05 02:06:07 -0400

    add handleClean

--- apps/client/src/pages/landing/sign-up/SignUpForm.tsx
+++ apps/client/src/pages/landing/sign-up/SignUpForm.tsx
@@ -2,14 +2,20 @@ import { InputSignUp } from "./components/InputsSignUp"
 import BackGroundSignUp from "./pictures/young-woman-reading.jpg"
 import { LocationSelector } from "./components/LocationSelectors"
 import {SuccessFullAlert} from "./components/SuccessFullAlert"
-import { useState} from "react"
+import { useState, useRef} from "react"
 
 function SignUp(){
 	const [registrationSuccess, setregistrationSuccss] = useState(false)
+	const formRef = useRef<HTMLFormElement>(null)
 	
+	function handleClean(){
+		formRef.current?.reset()
+	}
+
+
 	function handleSubmit(e: React.FormEvent<HTMLFormElement>){
 		e.preventDefault()
-		const fields = Object.fromEntries(new window.FormData(e.currentTarget))
+		const fields = Object.fromEntries(new window.FormData(e.currentTarget)) 
 		const { name, lastname, email, birthday, gender, direction, postal, password, country, city, state} = fields;
 		
 		let url = "http://localhost:3000/api/accounts/create";
@@ -32,8 +38,6 @@ function SignUp(){
 			customer
 		}
 
-		console.log(data)
-
 		fetch(url, {
 			method: "POST", 
 			body: JSON.stringify(data), 
@@ -46,7 +50,8 @@ function SignUp(){
 			.then((response) =>  {
 				console.log("Success:", response)
 				setregistrationSuccss(true)
-				setTimeout(()=> setregistrationSuccss(false), 10000) 
+				handleClean()
+				setTimeout(()=> setregistrationSuccss(false), 12000)
 			});
 
 
@@ -61,7 +66,7 @@ function SignUp(){
 				<div className="w-full h-full  flex justify-center items-center">
 					<div className="bg-cover w-1/2 h-1/2" >
 						<h2 className="text-2xl text-center text-white">Registro de Usuarios</h2>
-						<form className="" onSubmit={handleSubmit}>
+						<form ref={formRef} className="" onSubmit={handleSubmit}>
 							<div className="grid grid-cols-[1fr_1fr_1fr_1fr] grid-rows-[1fr_1fr_1fr_1fr_1fr_1fr] gap-2 text-white font-semibold">
 								<InputSignUp classNameLabel="flex flex-col text-center items-center w-full" classNameContainer="pl-2 col-span-2" 
 									valueLabel="Nombres" inputType="text" nameInput="name"
@@ -106,8 +111,8 @@ function SignUp(){
 
 							<div className="h-24 w-full mt-1 gap-1 flex flex-col justify-center items-center">
 								<div className="flex gap-2">
-									<button className="btn  btn-success btn-sm sm:btn-sm md:btn-md lg:btn-md text-white" >Registrate</button>
-									<button className="btn btn-accent btn-sm sm:btn-sm md:btn-md lg:btn-md text-white ">Limpiar</button>
+									<button type="button" className="btn btn-accent btn-sm sm:btn-sm md:btn-md lg:btn-md text-white" onClick={handleClean}>Limpiar</button>
+									<button type="submit" className="btn  btn-success btn-sm sm:btn-sm md:btn-md lg:btn-md text-white">Registrate</button>
 								</div>
 								<span className="text-white">¿Tienes una cuenta? <a href="" className="text-success">Inicia sesión aquí.</a></span>
 							</div>
