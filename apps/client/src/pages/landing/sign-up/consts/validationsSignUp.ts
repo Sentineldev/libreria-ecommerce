1b088ab781b29a27637c26620cb241625f89a512
Joel Guillen <noctis2001@gmail.com>
2024-08-05 17:19:50 -0400

    Adding typesOfSignUp, constSignUp, validations to Form and ErrorAlert

--- apps/client/src/pages/landing/sign-up/consts/validationsSignUp.ts
+++ apps/client/src/pages/landing/sign-up/consts/validationsSignUp.ts
@@ -1,74 +1,49 @@
+import { ItemsFormSignUp } from "../types/typesOfSignUp";
+import { INITIALY_DATA_CUSTOMER } from "./constSignUp";
+
 const nameAndLastNameRegExp = /^[a-zA-Z]+(?:\s+[a-zA-Z]+)*$/;
 const emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const postalCodeRegExp = /^[a-zA-Z0-9\s-]+$/;
 
-type FormSignUp = {
-    name: string,
-    lastname: string,
-    email: string,
-    birthday: string,
-    gender: string,
-    direction: string,
-    country: string,
-    state: string,  
-    city: string, 
-    postalCode: string,
-    password: string, 
-    confirmPassword: string
+function validateName(name: string): boolean {
+    return nameAndLastNameRegExp.test(name);
 }
 
-
-function validateName(name: string): boolean{
-    return nameAndLastNameRegExp.test(name)
+function validateLastname(lastName: string): boolean {
+    return nameAndLastNameRegExp.test(lastName);
 }
 
-function validateLastname(lastName: string):boolean{
-    return nameAndLastNameRegExp.test(lastName)
+function validateEmail(email: string): boolean {
+    return emailRegExp.test(email);
 }
 
-function validateEmail(email:string){
-    return emailRegExp.test(email)
+function validatePostalCode(postalCode: string): boolean {
+    return postalCodeRegExp.test(postalCode);
 }
 
-function validatePostalCode(codePostal: string):boolean{
-    return postalCodeRegExp.test(codePostal)
-}
-
-
-export function validationSignUp(data: FormSignUp){
-    const errors = {
-        name: "",
-        lastname: "",
-        email: "",
-        birthday: "",
-        gender: "",
-        direction: "",
-        country: "",
-        state: "",  
-        city: "", 
-        postalCode: "",
-        password: "", 
+export function validationSignUp(data: ItemsFormSignUp) {
+    // Crear una copia del objeto de errores inicial
+    const errors = { ...INITIALY_DATA_CUSTOMER };
+    
+    if (!validateName(data.name)) {
+        errors.name = "El nombre debe contener solo letras";
     }
 
-    if(!validateName(data.name)){
-        errors.name = "El nombre deben ser solo letras"
+    if (!validateLastname(data.lastname)) {
+        errors.lastname = "El apellido debe contener solo letras";
     }
 
-    if(!validateLastname(data.lastname)){
-        errors.name = "El apellido deben ser solo letras"
+    if (!validateEmail(data.email)) {
+        errors.email = "El correo electrónico no es válido";
     }
 
-    if(!validateEmail(data.email)){
-        errors.name = "El correo electrónico no es válido"
+    if (!validatePostalCode(data.postalCode)) {
+        errors.postalCode = "El código postal debe contener solo números o una combinación de números y letras";
     }
 
-    if(validatePostalCode(data.postalCode)){
-        errors.postalCode = "El código postal deben ser solo números o una combinación de números y letras"
+    if (data.password !== data.confirmPassword) {
+        errors.password = "Las contraseñas deben ser iguales.";
     }
 
-    if(data.password != data.confirmPassword){
-        errors.password = "Las contraseñas deben ser iguales."
-    }
-
-
+    return errors;
 }
